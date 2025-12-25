5faaa35d63afc9b137f8c53f2ebe38d249582ff2
dendi_yoker <denis.yuryevich.p@yandex.ru>
2025-12-25 22:09:21 +0300

    добработки после ревью

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,21 @@
 import model.Apple;
 import model.Food;
 import model.Meat;
+import model.constants.Colour;
 import service.ShoppingCart;
 
 public class Main {
     public static void main(String[] args) {
 
         Meat meat = new Meat(5, 100.0);
-        Apple redApple = new Apple(10, 50.0, "red");
-        Apple greenApple = new Apple(8, 60.0, "green");
+        Apple redApple = new Apple(10, 50.0, Colour.RED);
+        Apple greenApple = new Apple(8, 60.0, Colour.GREEN);
 
         Food[] foods = new Food[]{meat, redApple, greenApple};
 
         ShoppingCart sCart = new ShoppingCart(foods);
-        System.out.println("Стоимость всей корзины: " + sCart.getTotalPriceWithoutDiscount());
-        System.out.println("Стоимость коризины со скидкой: " + sCart.getTotalPriceWithDiscount());
-        System.out.println("Стоимость вегетарианских продкутов: " + sCart.getTotalVegetarianPriceWithoutDiscount());
+        System.out.printf("Стоимость коризины со скидкой: %s%n", sCart.getTotalPriceWithoutDiscount());
+        System.out.printf("Стоимость коризины со скидкой: %s%n", sCart.getTotalPriceWithDiscount());
+        System.out.printf("Стоимость вегетарианских продкутов: %s%n", sCart.getTotalVegetarianPriceWithoutDiscount());
     }
 }
